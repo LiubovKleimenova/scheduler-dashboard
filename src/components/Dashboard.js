6051333663938ce361d8c4bab9881ffecabc0013
LiubovKleimenova <kleimenovaliubov@gmail.com>
2020-03-26 14:00:04 -0400

    make info updated on db update

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -10,27 +10,28 @@ import {
 	getMostPopularDay,
 	getInterviewsPerDay
 } from "helpers/selectors";
+import { setInterview } from "helpers/reducers";
 
 const data = [
 	{
 		id: 1,
 		label: "Total Interviews",
-		value: 6
+		getValue: getTotalInterviews
 	},
 	{
 		id: 2,
 		label: "Least Popular Time Slot",
-		value: "1pm"
+		getValue: getLeastPopularTimeSlot
 	},
 	{
 		id: 3,
 		label: "Most Popular Day",
-		value: "Wednesday"
+		getValue: getMostPopularDay
 	},
 	{
 		id: 4,
 		label: "Interviews Per Day",
-		value: "2.3"
+		getValue: getInterviewsPerDay
 	}
 ];
 
@@ -44,8 +45,8 @@ class Dashboard extends Component {
 	};
 
 	componentDidMount() {
-    const focused = JSON.parse(localStorage.getItem("focused"));
-    Promise.all([
+		const focused = JSON.parse(localStorage.getItem("focused"));
+		Promise.all([
 			axios.get("/api/days"),
 			axios.get("/api/appointments"),
 			axios.get("/api/interviewers")
@@ -58,6 +59,18 @@ class Dashboard extends Component {
 			});
 		});
 
+    this.socket = new WebSocket(process.env.REACT_APP_WEBSOCKET_URL);
+    
+    this.socket.onmessage = event => {
+			const data = JSON.parse(event.data);
+
+			if (typeof data === "object" && data.type === "SET_INTERVIEW") {
+				this.setState(previousState =>
+					setInterview(previousState, data.id, data.interview)
+				);
+			}
+		};
+
 		if (focused) {
 			this.setState({ focused });
 		}
@@ -67,6 +80,10 @@ class Dashboard extends Component {
 		if (previousState.focused !== this.state.focused) {
 			localStorage.setItem("focused", JSON.stringify(this.state.focused));
 		}
+	}
+
+	componentWillUnmount() {
+		this.socket.close();
 	}
 
 	selectPanel(id) {
@@ -95,7 +112,7 @@ class Dashboard extends Component {
 					key={panel.id}
 					//id={panel.id}
 					label={panel.label}
-					value={panel.value}
+					value={panel.getValue(this.state)}
 					onSelect={event => this.selectPanel(panel.id)}
 				/>
 			));
