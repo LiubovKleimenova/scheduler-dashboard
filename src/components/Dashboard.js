78020deecd57389e162f460247e1ab026eba0e97
LiubovKleimenova <kleimenovaliubov@gmail.com>
2020-03-26 12:11:21 -0400

    added data to panels

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 
 import classnames from "classnames";
 import Loading from "./Loading";
+import Panel from "./Panel";
 
 const data = [
 	{
@@ -27,16 +28,27 @@ const data = [
 ];
 
 class Dashboard extends Component {
-  state = { loading: false };
-  
-	render() {
-    const dashboardClasses = classnames("dashboard");
+	state = {
+		loading: false
+	};
 
-    if (this.state.loading) {
+	render() {
+		const dashboardClasses = classnames("dashboard");
+
+		if (this.state.loading) {
 			return <Loading />;
 		}
 
-		return <main className={dashboardClasses} />;
+		const panels = data.map(panel => (
+			<Panel
+				key={panel.id}
+				id={panel.id}
+				label={panel.label}
+				value={panel.value}
+			/>
+		));
+
+		return <main className={dashboardClasses}>{panels}</main>;
 	}
 }
 
